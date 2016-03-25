665930702af52f841665f4f04910d09c7ea07df8
keaka1999 <alexkeaka@yahoo.com>
2016-03-24 23:09:25 -0500

    1.8.9

---
+++ src/main/java/net/bcc/biochronicraft/client/ClientProxy.java
@@ -0,0 +1,20 @@
+package net.bcc.biochronicraft.client;
+
+import net.bcc.biochronicraft.server.ServerProxy;
+
+public class ClientProxy extends ServerProxy {
+    @Override
+    public void onPreInit() {
+
+    }
+
+    @Override
+    public void onInit() {
+
+    }
+
+    @Override
+    public void onPostInit() {
+
+    }
+}
